cf4fb07be67101a79ea8d3ba91caec39be80c771
Wojciech Gąsiewicz <235086@student.pwr.edu.pl>
2021-03-21 00:04:17 +0100

    info page added

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HamburgerMenu from './HamburgerMenu';
+import '../styles/Info.css';
 
 class Info extends Component {
     constructor(props) {
@@ -8,9 +9,28 @@ class Info extends Component {
 
     render() {
         return (
-            <HamburgerMenu/>
+            <div>
+                <HamburgerMenu />
+                <div className="login-container">
+                    <div className="form-box-vin">
+                        <div className="body-form">
+                            <h2 className="text-header">Header</h2>
+                            <ColoredLine color="white" height={5} />
+                            <p className="text-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
-
+const ColoredLine = ({ color, height }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: height
+        }}
+    />
+);
 export default Info;
